e0a973f62bca8e1dae3cb5c336ad76d38f8ef0af
Daniel Brosnan Blázquez <d.brosnan.b@gmail.com>
2018-05-31 14:11:07 +0200

    Include event links data model.
    Correct relationships with other entities of the data model

--- src/datamodel/events.py
+++ src/datamodel/events.py
@@ -6,7 +6,7 @@ Written by DEIMOS Space S.L. (dibb)
 module gsdm
 """
 
-from sqlalchemy import Column, Integer, Table, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, Table, DateTime, ForeignKey, Text
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import relationship
 
@@ -21,16 +21,32 @@ class Event(Base):
     time_stamp = Column(DateTime)
     ingestion_time = Column(DateTime)
     gauge_id = Column(Integer, ForeignKey('gauge_cnf_tb.gauge_id'))
-    #explicit_ref_id = Column(Integer, ForeignKey('explicit_ref_tb.explicit_ref_id'))
-    #processing_uuid = Column(postgresql.UUID, ForeignKey('dim_processing_tb.processing_uuid'))
+    explicit_ref_id = Column(Integer, ForeignKey('explicit_ref_tb.explicit_ref_id'))
+    processing_uuid = Column(postgresql.UUID(as_uuid=True), ForeignKey('dim_processing_tb.processing_uuid'))
     gauge = relationship("Gauge", backref="events")
+    explicitRef = relationship("ExplicitRef", backref="events")
+    source = relationship("DimProcessing", backref="events")
 
-    def __init__(self, eventUuid, start, stop, timeStamp, ingestionTime, gauge, explicitRefId = None, processingUuid = None):
+    def __init__(self, eventUuid, start, stop, timeStamp, ingestionTime, gauge, explicitRef = None, dimProcessing = None):
         self.event_uuid = eventUuid
         self.start = start
         self.stop = stop
         self.time_stamp = timeStamp
         self.ingestion_time = ingestionTime
         self.gauge = gauge
-        self.explicit_ref_id = explicitRefId
-        self.processing_uuid = processingUuid
+        self.explicitRef = explicitRef
+        self.source = dimProcessing
+
+class EventLink(Base):
+    __tablename__ = 'event_links_tb'
+
+    event_uuid_link = Column(postgresql.UUID(as_uuid=True), primary_key=True)
+    name = Column(Text)
+    event_uuid = Column(postgresql.UUID(as_uuid=True), ForeignKey('event_tb.event_uuid'))
+    event = relationship("Event", backref="eventLinks")
+
+    def __init__(self, link, name, event):
+        self.event_uuid_link = link
+        self.name = name
+        self.event = event
+
