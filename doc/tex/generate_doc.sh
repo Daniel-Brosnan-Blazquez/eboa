41d11bbd07eef9dec9f32a4b51cd7606e4b66d6b
Daniel Brosnan Blázquez <d.brosnan.b@gmail.com>
2018-09-07 13:42:13 +0200

    Add automatic insertion of the code documentation extracted from the docstrings of the Python code
    Add vhistory support for generating version history registers

--- doc/tex/generate_doc.sh
+++ doc/tex/generate_doc.sh
@@ -10,6 +10,13 @@ USAGE="Usage: `basename $0` -f pdf_file [-k]\n
 Optional parameters:\n
 -k: is set it indicates the script to keep the build directory
 "
+if [ -z "$GSDM_RESOURCES_PATH" ];
+then
+    echo -e "ERROR: The environment variable GSDM_RESOURCES_PATH has to be defined"
+    echo -e $USAGE
+    exit -1
+fi
+
 PDF_FILE=""
 KEEP_BUILD="NO"
 
@@ -38,6 +45,14 @@ then
     rm -rf build
 fi
 mkdir build
+
+# Automatically Extract the documentation from the python code
+# Generate the tex file
+sphinx-build -b latex ../../src/docs/source/ build
+# Extract the chapter describing the code
+sed -n '/chapter{Subpackages}/,/chapter{Module contents}/p' build/GSDM_module.tex |head -n -1 > gsdm_code_documentation.tex
+# Replace the chapter text
+sed -i 's/chapter{Subpackages}/chapter{GSDM code documentation}/' gsdm_code_documentation.tex
 
 # Execute the first translation of the tex files into a PDF file
 pdflatex -output-directory build -halt-on-error -interaction=nonstopmode doc.tex &> /dev/null
