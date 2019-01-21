4e9196f55449d6b72a7d262169146bcab7262a2a
Daniel Brosnan Blázquez <d.brosnan.b@gmail.com>
2019-01-21 18:03:59 +0100

    Add script for initializing the DDBB provided a name

---
+++ datamodel/init_provided_ddbb.sh
@@ -0,0 +1,96 @@
+#################################################################
+#
+# Init DDBB of the eboa
+#
+# Written by DEIMOS Space S.L. (dibb)
+#
+# module eboa
+#################################################################
+USAGE="Usage: `basename $0` -f datamodel_file -d ddbb -p port -h host"
+DATAMODEL_FILE=""
+PORT="5432"
+HOST="localhost"
+
+while getopts f:d:p:h: option
+do
+    case "${option}"
+        in
+        f) DATAMODEL_FILE=${OPTARG};;
+        d) DDBB=${OPTARG};;
+        p) PORT=${OPTARG};;
+        h) HOST=${OPTARG};;
+        ?) echo -e $USAGE
+            exit -1
+    esac
+done
+
+# Check that the init of the DDBB is being executed by Postgres
+if [ "$(whoami)" != "postgres" ] && [ "$(whoami)" != "root" ]; then
+        echo "ERROR: Script must be run as user: postgres"
+        exit -1
+fi
+
+# Check that option -f has been specified
+if [ "$DATAMODEL_FILE" == "" ];
+then
+    echo "ERROR: The option -f has to be provided"
+    echo $USAGE
+    exit -1
+fi
+
+# Check that option -d has been specified
+if [ "$DDBB" == "" ];
+then
+    echo "ERROR: The option -d has to be provided"
+    echo $USAGE
+    exit -1
+fi
+
+# Check that the sql file for filling up the DDBB exists
+if [ ! -f $DATAMODEL_FILE ];
+then
+    echo "ERROR: The file $DATAMODEL_FILE provided does not exist"
+    exit -1
+fi
+
+# Check that there are no connections to the DDBB if exists
+DATABASE=`psql -p "$PORT" -h "$HOST" -t -U postgres -c "SELECT count(*) FROM pg_database WHERE datname='$DDBB'";`
+if [ $DATABASE -eq 1 ];
+then
+    CONNECTIONS=`psql -p "$PORT" -h "$HOST" -U postgres -t -c "SELECT count(*) FROM pg_stat_activity where datname = '$DDBB';"`
+    if [ $CONNECTIONS -ne 0 ];
+    then
+        echo "ERROR: There are $((CONNECTIONS + 0)) active connections to the DDBB"
+        exit -1
+    fi
+fi
+
+# Remove DDBB if it exists
+if [ $DATABASE -eq 1 ];
+then
+    # Drop the DDBB
+    psql -p $PORT -h $HOST -U postgres -c "DROP DATABASE $DDBB;"
+
+    # Drop the eboa role
+    psql -p $PORT -h $HOST -U postgres -c "DROP ROLE eboa;"
+fi
+
+# Create DDBB
+psql -p $PORT -h $HOST -U postgres -c "CREATE DATABASE $DDBB;"
+
+# Add extenstion for postgis
+psql -p $PORT -h $HOST -U postgres -d $DDBB -c "CREATE EXTENSION postgis;"
+
+# Fill DDBB
+psql -p $PORT -h $HOST -U postgres -d $DDBB -f $DATAMODEL_FILE
+status=$?
+
+if [ $status -ne 0 ];
+then
+    echo "ERROR: It was not possible to fill up the DDBB"
+    exit -1
+fi
+
+echo "DDBB has been initiated correctly!"
+
+exit 0
