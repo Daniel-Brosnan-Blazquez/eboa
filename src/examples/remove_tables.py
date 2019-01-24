004ecb4bd2ce5ac886b84a959a6d9edf9f534de8
Felix Munoz <felix.munoz@elecnor.es>
2019-01-24 10:29:38 +0100

    Fix datamodel to adapt the code to the new names

--- src/examples/remove_tables.py
+++ src/examples/remove_tables.py
@@ -12,7 +12,7 @@ from eboa.datamodel.base import Session, engine, Base
 from eboa.datamodel.dim_signatures import DimSignature
 from eboa.datamodel.events import Event, EventLink, EventKey, EventText, EventDouble, EventObject, EventGeometry
 from eboa.datamodel.gauges import Gauge
-from eboa.datamodel.dim_processings import DimProcessing, DimProcessingStatus
+from eboa.datamodel.sources import Source, SourceStatus
 from eboa.datamodel.explicit_refs import ExplicitRef, ExplicitRefGrp, ExplicitRefLink
 from eboa.datamodel.annotations import Annotation, AnnotationCnf, AnnotationText, AnnotationDouble, AnnotationObject, AnnotationGeometry
 
