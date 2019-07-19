4d7281dd81f2b648b7d089a55280e8e6256ea3f0
Daniel Brosnan <d.brosnan.b@gmail.com>
2019-07-19 15:22:38 +0200

    Version incremented to 0.1.1

--- src/setup.py
+++ src/setup.py
@@ -8,7 +8,7 @@ module eboa
 from setuptools import setup, find_packages
 
 setup(name="eboa",
-      version="0.1.0",
+      version="0.1.1",
       description="Engine for Business Operation Analysis",
       url="https://bitbucket.org/dbrosnan/eboa/",
       author="Daniel Brosnan",
