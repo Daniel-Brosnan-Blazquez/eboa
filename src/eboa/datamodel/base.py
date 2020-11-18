06ccffc5ff081363cd91fe8b84d04575cbb44bfb
dibb <daniel.brosnan@deimos-space.com>
2020-11-18 11:57:19 +0100

    Add access to the DDBB host through environment variables to allow changing the host during the installation process

--- src/eboa/datamodel/base.py
+++ src/eboa/datamodel/base.py
@@ -6,6 +6,7 @@ Written by DEIMOS Space S.L. (dibb)
 module eboa
 """
 # Import SQLalchemy entities
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -16,6 +17,10 @@ from eboa.datamodel.functions import read_configuration
 config = read_configuration()
 
 db_configuration = config["DDBB_CONFIGURATION"]
+
+if "EBOA_DDBB_HOST" in os.environ:
+    db_configuration["host"] = os.environ["EBOA_DDBB_HOST"]
+# end if
 
 db_uri = "{db_api}://{user}@{host}:{port}/{database}".format(**db_configuration)
 
