e0c9d7e8859da6519babef6ae19799222c137a48
Daniel Brosnan <daniel.brosnan@elecnor.es>
2021-08-26 17:41:23 +0200

    Add first consolidated version of the UBOA data model

--- src/uboa/datamodel/users.py
+++ src/uboa/datamodel/users.py
@@ -5,7 +5,7 @@ Written by DEIMOS Space S.L. (dibb)
 
 module uboa
 """
-from eboa.datamodel.base import Base
+from uboa.datamodel.base import Base
 from flask_security import UserMixin, RoleMixin
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Table, DateTime, ForeignKey, Text, Integer, Boolean
@@ -13,21 +13,32 @@ from sqlalchemy.dialects import postgresql
 
 class RoleUser(Base):
     __tablename__ = 'roles_users'
-    id = Column(Integer(), primary_key=True)
-    user_uuid = Column(Integer(), ForeignKey('users.id'))
-    role_uuid = Column(Integer(), ForeignKey('roles.id'))
+    role_user_uuid = Column(postgresql.UUID(as_uuid=True), primary_key=True)
+    user_uuid = Column(postgresql.UUID(as_uuid=True), ForeignKey('users.user_uuid'))
+    role_uuid = Column(postgresql.UUID(as_uuid=True), ForeignKey('roles.role_uuid'))
 
+    def __init__(self, role_user_uuid, user_uuid, role_uuid):
+        self.role_user_uuid = role_user_uuid
+        self.user_uuid = user_uuid
+        self.role_uuid = role_uuid
+        
 class Role(Base, RoleMixin):
     __tablename__ = 'roles'
-    id = Column(Integer(), primary_key=True)
+    role_uuid = Column(postgresql.UUID(as_uuid=True), primary_key=True)
     name = Column(Text)
     description = Column(Text)
 
+    def __init__(self, role_uuid, name, description = None):
+        self.role_uuid = role_uuid
+        self.name = name
+        self.description = description
+
 class User(Base, UserMixin):
     __tablename__ = 'users'
-    id = Column(Integer(), primary_key=True)
+    user_uuid = Column(postgresql.UUID(as_uuid=True), primary_key=True)
     email = Column(Text)
     username = Column(Text)
+    group = Column(Text)
     password = Column(Text)
     last_login_at = Column(DateTime)
     current_login_at = Column(DateTime)
@@ -38,3 +49,15 @@ class User(Base, UserMixin):
     fs_uniquifier = Column(Text)
     confirmed_at = Column(DateTime)
     roles = relationship("Role", secondary="roles_users", backref="users")
+
+    def __init__(self, user_uuid, email, username, group, password, fs_uniquifier, active = True):
+        """
+        User is active by default
+        """
+        self.user_uuid = user_uuid
+        self.email = email
+        self.username = username
+        self.group = group
+        self.password = password
+        self.fs_uniquifier = fs_uniquifier
+        self.active = active
