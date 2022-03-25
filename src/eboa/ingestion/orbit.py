33506358048844619ff19b7ce05e529f12019030
Daniel Brosnan Blázquez <daniel.brosnan@elecnor.es>
2022-03-25 10:08:14 +0100

    Add module to manage the orbit of a satellite given its TLE

---
+++ src/eboa/ingestion/orbit.py
@@ -0,0 +1,60 @@
+"""
+Helper module for managing satellite orbits in the EBOA component
+
+Written by DEIMOS Space S.L. (dibb)
+
+module eboa
+"""
+# Import python utilities
+from dateutil import parser
+import datetime
+from lxml import etree, objectify
+import re
+
+# Import SGP4 utilities for orbit propagation
+from sgp4.earth_gravity import wgs72
+from sgp4.io import twoline2rv
+
+# Import eboa utilities
+from eboa.engine.errors import IncorrectTle
+
+###########
+# Functions for managing the orbit of a satellite
+###########
+def get_orbit(tle):
+    """
+    Method to obtain the orbit of a satellite from its TLE
+    :param tle: TLE of the satellite with the following format:
+    SATELLITE-INDICATOR
+    1 NNNNNC NNNNNAAA NNNNN.NNNNNNNN +.NNNNNNNN +NNNNN-N +NNNNN-N N NNNNN
+    2 NNNNN NNN.NNNN NNN.NNNN NNNNNNN NNN.NNNN NNN.NNNN NN.NNNNNNNNNNNNNN
+    :type tle: str
+
+    :return: orbit
+    :rtype: sgp4.model.Satellite
+
+    """
+
+    # Verify TLE has three lines
+    if not re.match(".*\n.*\n.*", tle):
+        raise IncorrectTle(tle)
+    # end if
+
+    # Obtain first line of TLE
+    first_line = tle.split("\n")[1]
+
+    # Verify first line 
+    if not re.match("1 ...... ........ .............. .......... ......-. ......-. . .....", first_line):
+        raise IncorrectTle(tle)
+    # end if
+
+    second_line = tle.split("\n")[2]
+
+    # Verify second line 
+    if not re.match("2 ..... ........ ........ ....... ........ ........ .................", second_line):
+        raise IncorrectTle(tle)
+    # end if
+
+    orbit = twoline2rv(first_line, second_line, wgs72)
+
+    return orbit
