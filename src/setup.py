4d2e88e280afa80d571a5e14131608eec8cb58a7
Daniel Brosnan Blázquez <daniel.brosnan@elecnor.es>
2023-08-29 14:06:59 +0200

    Increase version to 1.0.3

--- src/setup.py
+++ src/setup.py
@@ -8,7 +8,7 @@ module eboa
 from setuptools import setup, find_packages
 
 setup(name="eboa",
-      version="1.0.2",
+      version="1.0.3",
       description="Engine for Business Operation Analysis",
       url="https://bitbucket.org/dbrosnan/eboa/",
       author="Daniel Brosnan",
