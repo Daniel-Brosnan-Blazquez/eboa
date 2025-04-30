f6d0acbe7a678899dbd21f357b3570743eb46152
Daniel Brosnan Blázquez <daniel.brosnan@elecnor.es>
2025-04-30 16:35:17 +0200

    Remove initialization of the BOA DDBBs from the script to start EBOA for testing with github actions
    Add script to initialize the BOA DDBBs

---
+++ src/scripts/initialize_eboa_ddbb.sh
@@ -0,0 +1,20 @@
+# Start EBOA for running infinitely
+echo
+echo "####################"
+echo "Initialize EBOA DDBB"
+echo "####################"
+while true
+do
+    echo "Trying to initialize EBOA, SBOA, minArc and ORC databases..."
+    boa_init.py -e -s -u -y
+    status=$?
+    if [ $status -ne 0 ]
+    then
+        echo "Server is not ready yet..."
+        # Wait for the server to be initialize
+        sleep 1
+    else
+        echo "Databases have been initialized... :D"
+        break
+    fi
+done
